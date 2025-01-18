4c531898e5d34c82085557878b9334f2f78732ad
Williams de SOUZA <williams.stanley.desouza@gmail.com>
2025-01-18 16:43:07 +0100

    site web esperance des batisseurs

---
+++ wordpress-files/wp-content/plugins/gutenkit-blocks-addon/build/gutenkit/components.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'd6a70c9ac85641bcf6b1');
